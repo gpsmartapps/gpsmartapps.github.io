14a85ed41746bf3e14482534ca3737c50aee26d7
Ogwo GP <ogwogp@outlook.com>
2022-11-17 19:22:53 +0100

    Update creator.js

--- SmartCorrect/src/js/creator.js
+++ SmartCorrect/src/js/creator.js
@@ -7,15 +7,13 @@ window.smartsupp || (function (d) {
     c.src = 'https://www.smartsuppchat.com/loader.js?'; s.parentNode.insertBefore(c, s);
 })(document);
 
-// < !--HoverSignal -->
-// (function (d, w) {
-// var n = d.getElementsByTagName("script")[0],
-// s = d.createElement("script"),
-// f = function () { n.parentNode.insertBefore(s, n); };
-// s.type = "text/javascript";
-// s.async = true;
-// s.src = "https://app.hoversignal.com/Api/Script/2e09a900-28ad-494a-aad2-5192c5ac4cfa";
-// if (w.opera == "[object Opera]") {
-// d.addEventListener("DOMContentLoaded", f, false);
-// } else { f(); }
-// })(document, window);
+// < !--Smartsupp Live Chat script-- >
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '85aa0baa2e938b54039081ddaf2473c4bff05278';
+window.smartsupp || (function (d) {
+    var s, c, o = smartsupp = function () { o._.push(arguments) }; o._ = [];
+    s = d.getElementsByTagName('script')[0]; c = d.createElement('script');
+    c.type = 'text/javascript'; c.charset = 'utf-8'; c.async = true;
+    c.src = 'https://www.smartsuppchat.com/loader.js?'; s.parentNode.insertBefore(c, s);
+})(document);
+{/* </script> */ }
