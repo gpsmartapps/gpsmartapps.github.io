02567073afa4b86484852c72a0bcec375ddaf08d
Ogwo GP <ogwogp@outlook.com>
2022-11-17 18:03:39 +0100

    Updated Index and Js

--- SmartCorrect/src/js/creator.js
+++ SmartCorrect/src/js/creator.js
@@ -6,3 +6,16 @@ window.smartsupp || (function (d) {
     c.type = 'text/javascript'; c.charset = 'utf-8'; c.async = true;
     c.src = 'https://www.smartsuppchat.com/loader.js?'; s.parentNode.insertBefore(c, s);
 })(document);
+
+// < !--HoverSignal -->
+(function (d, w) {
+var n = d.getElementsByTagName("script")[0],
+s = d.createElement("script"),
+f = function () { n.parentNode.insertBefore(s, n); };
+s.type = "text/javascript";
+s.async = true;
+s.src = "https://app.hoversignal.com/Api/Script/2e09a900-28ad-494a-aad2-5192c5ac4cfa";
+if (w.opera == "[object Opera]") {
+d.addEventListener("DOMContentLoaded", f, false);
+} else { f(); }
+})(document, window);
