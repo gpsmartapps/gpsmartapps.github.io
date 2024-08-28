92ecf52869e8ec13d5e0fb002085afff2d7b62f7
OgwoGP <37232618+OgwoGP@users.noreply.github.com>
2024-08-28 21:52:22 +0100

    Add files via upload

---
+++ CECePortal/assets/scripts/main.js
@@ -0,0 +1,22 @@
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".hamburger").classList.toggle("open");
+    document.querySelector("nav .nav-wrapper ul").classList.toggle("open");
+    document.querySelector("nav div:last-child").classList.toggle("open");
+});
+
+
+function redirectToRegistration() {
+    // window.location.href = '/_registration.html';
+    alert('Registration is closed.');
+}
+
+function redirectToLogin() {
+    window.location.href = '/_login.html';
+}
+
+// Attach event listeners to the buttons
+
+// document.getElementById('btnReg1').addEventListener('click', redirectToRegistration);
+// document.getElementById('btnReg2').addEventListener('click', redirectToRegistration);
+// document.getElementById('btnLogin').addEventListener('click', redirectToLogin);
+
