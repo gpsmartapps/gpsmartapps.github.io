889ef502c316f027c02e8f68f955d56584651f3b
OgwoGP <ogwogp@gmail.com>
2024-08-29 12:13:04 +0100

    Updated all

---
+++ SmartCorrect/src/js/creator.js
@@ -0,0 +1,19 @@
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '85aa0baa2e938b54039081ddaf2473c4bff05278';
+window.smartsupp || (function (d) {
+    var s, c, o = smartsupp = function () { o._.push(arguments) }; o._ = [];
+    s = d.getElementsByTagName('script')[0]; c = d.createElement('script');
+    c.type = 'text/javascript'; c.charset = 'utf-8'; c.async = true;
+    c.src = 'https://www.smartsuppchat.com/loader.js?'; s.parentNode.insertBefore(c, s);
+})(document);
+
+// < !--Smartsupp Live Chat script-- >
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '85aa0baa2e938b54039081ddaf2473c4bff05278';
+window.smartsupp || (function (d) {
+    var s, c, o = smartsupp = function () { o._.push(arguments) }; o._ = [];
+    s = d.getElementsByTagName('script')[0]; c = d.createElement('script');
+    c.type = 'text/javascript'; c.charset = 'utf-8'; c.async = true;
+    c.src = 'https://www.smartsuppchat.com/loader.js?'; s.parentNode.insertBefore(c, s);
+})(document);
+{/* </script> */ }
