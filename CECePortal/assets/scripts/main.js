8be0a4512e083d8a81a1718e730a946e4b298f38
OgwoGP <ogwogp@gmail.com>
2024-08-30 01:01:17 +0100

    Updated

--- CECePortal/assets/scripts/main.js
+++ CECePortal/assets/scripts/main.js
@@ -1,5 +1,3 @@
-console.log("Script loaded successfully");
-
 document.querySelector(".hamburger").addEventListener("click", () => {
     document.querySelector(".hamburger").classList.toggle("open");
     document.querySelector("nav .nav-wrapper ul").classList.toggle("open");
